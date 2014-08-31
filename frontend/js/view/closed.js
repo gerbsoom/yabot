c91da30093f18992e1f3c20bd48eeedefd4b7765
gerbsoom <gerbsoom@web.de>
2014-08-31 18:39:30 +0200

    Nice up the useless closed view.

--- frontend/js/view/closed.js
+++ frontend/js/view/closed.js
@@ -1,24 +1,35 @@
 var closedImage, bgImage;
 var closedCanvas;
-var offsetBGY = -300;
-var offsetX = 100;
+var closedTimer;
+var offsetBGY = -2000;
+var offsetX = 350;
 var modifier = 1;
 var bgModifier = 1;
 
 function generateClosedTabPanel()
 {
-    var closedTabPanel = generateTabPanelDiv("tabPanel", "closedTabPanel", null, 800, 300);
+    var closedTabPanel = generateTabPanelDiv("tabPanel", "closedTabPanel", null, 1000, 300);
 
     var closedCanvasComponent = generateDiv("component", "closedCanvasComponent");
-    closedCanvas = generateCanvas("closedCanvas", 800, 300);
+    closedCanvas = generateCanvas("closedCanvas", 1000, 300);
     closedCanvasComponent.appendChild(closedCanvas);
     addElementToComponent(closedTabPanel, closedCanvas);
 
     bgImage = createImage("img/closed_background.png");
     closedImage = createImage("img/closed.png");
-    closedImage.onload = setInterval(drawClosedCanvas, 50);
+    closedImage.onload = startClosedAnimation;
 
     return closedTabPanel;
+}
+
+function startClosedAnimation()
+{
+    closedTimer = setInterval(drawClosedCanvas, 50);
+}
+
+function stopClosedAnimation()
+{
+    clearInterval(closedTimer);
 }
 
 function drawClosedCanvas()
@@ -29,11 +40,11 @@ function drawClosedCanvas()
         ctx.clearRect(0, 0, 800, 300);
         ctx.drawImage(bgImage, 0, offsetBGY);
         ctx.drawImage(closedImage, offsetX, 0);
-        if (offsetX > 200) modifier *= -1;
+        if (offsetX > 700) modifier *= -1;
         else if (offsetX < 0)  modifier *= -1;
         offsetX += 2*modifier;
         if (offsetBGY > 0) bgModifier *= -1;
-        else if (offsetBGY < -300)  bgModifier *= -1;
+        else if (offsetBGY < -2000)  bgModifier *= -1;
         offsetBGY += 3*bgModifier;
     }
 }
